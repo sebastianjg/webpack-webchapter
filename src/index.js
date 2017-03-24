436e42de27615e5b35b7ace29ad734e85bf0ba44
Sebastian Jara <sebastian.jara@knowit.no>
2017-03-24 12:23:45 +0100

    Minimal Webpack config

---
+++ src/index.js
@@ -0,0 +1,3 @@
+// index.js
+const root = document.querySelector('#root');
+root.innerHTML = '<h1>Hello Webpack!</h1>';
---
+++ webpack.config.js
@@ -0,0 +1,11 @@
+// webpack.config.js
+const webpack = require('webpack')
+
+const config = {
+  entry: './src/index.js',
+  output: {
+    filename: './dist/bundle.js'
+  }
+}
+
+module.exports = config
